6944f65e4157c23d7188cf1ea3cbb06001dd0a25
Jan Valenta <50635509+janvalentacz@users.noreply.github.com>
2022-01-01 01:00:28 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello in 2022.");
+
+// CZ (Česká republika):  Přeji všem šťastný Nový rok 2022
+// EN (Great Britain): I wish you all a happy new year 2022
+// DE (Bundesrepublik Deutschland):  Ich wünsche euch allen einen guten Rutsch ins neue Jahr 2022. 
